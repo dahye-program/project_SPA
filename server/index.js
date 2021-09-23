a159b784263c0530744e2843dca1a628bbe82155
dahye-program <dahye0729@sch.ac.kr>
2021-09-24 03:20:26 +0900

    [UPDATE] DB Connect

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mysql = require('mysql');
 const fs = require('fs');
 const imageToBase64 = require('image-to-base64');
 
@@ -10,33 +11,38 @@ app.use(cors({
 }))
 
 // 이미지 저장해서 불러오는 것
-app.get('/test', (async (req, res)=>{
-    console.info("반갑다 친구! 왔구나! ");
-    const MAX_INDEX = 50000;
-    const IMAGE_INDEX = 3;
-    /*
-    * 기존에 데이터 싹 날리는 로직
-    * */
+app.get('/test', (async (req, res) => {
+        console.info("반갑다 친구! 왔구나! ");
+        const MAX_INDEX = 50000;
+        const IMAGE_INDEX = 3;
 
-    for(let i = 0 ; i < MAX_INDEX; i++) {
-        let randomIndex = 1;
-        /*
-        * 난수 생성 로직
-        * */
-        const result = await imageToBase64(`./img/${randomIndex}.png`);
-        // console.log(result);
+        // DB Connection
+        const con = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "",
+            database: "aws_test"
+        });
 
-        /*
-        * DB INSERT logic
-        * */
-    }
-    res.send("어서오세요");
-}))
+        // DB 기존 데이터 싹 날리는 로직
+        for (let i = 0; i < MAX_INDEX; i++) {
+            let randomIndex = 1;
+            // 난수 생성 로직
 
-// app.post('/test',((req, res)=>{
-//     console.log(req.body);
-// }))
+            // 서버의 이미지 Base64 형식으로 불러오기
+            const result = await imageToBase64(`./img/${randomIndex}.png`);
 
-app.listen(3000, ()=>{
+            // 이미지 DB에 INSERT
+            con.connect(function (err) {
+                if (err) throw err;
+                console.log("Connected!");
+                // const sql = "INSERT INTO userinfo(ID, PW, Name, Sex) VALUES()";
+            })
+        }
+        res.send("어서오세요");
+    })
+)
+
+app.listen(3000, () => {
     console.log("Server On");
 })
