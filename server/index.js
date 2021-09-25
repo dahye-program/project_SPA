47025d4e6a26a05143292218c38fd97b54b8a958
dahye-program <dahye0729@sch.ac.kr>
2021-09-25 23:36:42 +0900

    [갈길이 멀다]

--- server/index.js
+++ server/index.js
@@ -22,12 +22,15 @@ app.get('/test', (async (req, res) => {
             user: "root",
             password: "",
             database: "aws_test"
+            // table이름: img64 로 할건데..
         });
 
         // DB 기존 데이터 싹 날리는 로직
+        // TRUNCATE TABLE img64 : img64 테이블 안의 데이터 모두 삭제
+
         for (let i = 0; i < MAX_INDEX; i++) {
-            let randomIndex = 1;
-            // 난수 생성 로직
+            // 1부터 3사이의 난수 생성 로직
+            let randomIndex = Math.floor(Math.random()*3)+1;
 
             // 서버의 이미지 Base64 형식으로 불러오기
             const result = await imageToBase64(`./img/${randomIndex}.png`);
@@ -36,7 +39,7 @@ app.get('/test', (async (req, res) => {
             con.connect(function (err) {
                 if (err) throw err;
                 console.log("Connected!");
-                // const sql = "INSERT INTO userinfo(ID, PW, Name, Sex) VALUES()";
+                // const sql = "INSERT INTO img64(value) VALUES(result)";
             })
         }
         res.send("어서오세요");
