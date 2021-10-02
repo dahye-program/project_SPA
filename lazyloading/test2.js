de952effd3b353907947a50a9460b1dd8f2e709e
dahye-program <dahye0729@sch.ac.kr>
2021-10-02 23:31:09 +0900

    [UPDATE] 스크롤하면 랜덤하게 이미지 출력git add .

--- lazyloading/test2.js
+++ lazyloading/test2.js
@@ -1,50 +1,35 @@
-const increasingColor = "rgba(40, 40, 190, ratio)"
-const decreasingColor = "rgba(190, 40, 40, ratio)"
+window.addEventListener("load", () => {
+    const test = new Test();
+});
 
-window.addEventListener("load",()=>{
-    console.log("되냐?");
-    const test = new Test();});
-
-class Test{
-    boxElement
+class Test {
+    boxElement = [];
     prevRatio
+
     constructor() {
-        console.log("Object");
-        this.boxElement = document.querySelector('#box');
-        this.prevRatio = 0.0
+        this.boxElement = document.querySelectorAll('.image');
+        this.prevRatio = 0.0;
         this.createObserver();
     }
-    createObserver(){
+
+    createObserver() {
         let observer;
         let options = {
-            root : null,
-            rootMargin :"0px",
-            threshold: this.buildThresholdList()
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.8
         };
         observer = new IntersectionObserver(this.handleIntersect, options);
-        observer.observe(this.boxElement);
+        this.boxElement.forEach(image => observer.observe(image));
     }
 
-    handleIntersect(entries, observer){
+    handleIntersect(entries, observer) {
         entries.forEach((entry) => {
-            if (entry.intersectionRatio > this.prevRatio) {
-                entry.target.style.backgroundColor = increasingColor.replace("ratio", entry.intersectionRatio);
-            } else {
-                entry.target.style.backgroundColor = decreasingColor.replace("ratio", entry.intersectionRatio);
+            if(entry.isIntersecting){
+                let randomIndex = Math.floor(Math.random() * 5) + 1;
+                entry.target.setAttribute('src', `./img/${randomIndex}.jpg`.concat());
             }
-            this.prevRatio = entry.intersectionRatio;
+
         });
-    }
-
-    buildThresholdList() {
-        let thresholds = [];
-        let numSteps = 20;
-
-        for (let i=1; i<=numSteps; i++) {
-            let ratio = i/numSteps;
-            thresholds.push(ratio);
-        }
-        thresholds.push(0);
-        return thresholds;
     }
 }
