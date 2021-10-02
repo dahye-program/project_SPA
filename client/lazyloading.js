8689931b324df9ac42cd3038b650514779535a95
dahye-program <dahye0729@sch.ac.kr>
2021-10-02 23:35:08 +0900

    [UPDATE] 스크롤 하면 이미지 랜덤하게 출력git add .

--- client/lazyloading.js
+++ client/lazyloading.js
@@ -1,29 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyloadImages = document.querySelectorAll("img.lazy");
-    var lazyloadThrottleTimeout;
+window.addEventListener("load", () => {
+    const test = new Test();
+});
 
-    function lazyload () {
-        if(lazyloadThrottleTimeout) {
-            clearTimeout(lazyloadThrottleTimeout);
-        }
+class Test {
+    boxElement = [];
+    prevRatio
 
-        lazyloadThrottleTimeout = setTimeout(function() {
-            var scrollTop = window.pageYOffset;
-            lazyloadImages.forEach(function(img) {
-                if(img.offsetTop < (window.innerHeight + scrollTop)) {
-                    img.src = img.dataset.src;
-                    img.classList.remove('lazy');
-                }
-            });
-            if(lazyloadImages.length == 0) {
-                document.removeEventListener("scroll", lazyload);
-                window.removeEventListener("resize", lazyload);
-                window.removeEventListener("orientationChange", lazyload);
-            }
-        }, 20);
+    constructor() {
+        this.boxElement = document.querySelectorAll('.image');
+        this.prevRatio = 0.0;
+        this.createObserver();
     }
 
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
-});
+    createObserver() {
+        let observer;
+        let options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.8
+        };
+        observer = new IntersectionObserver(this.handleIntersect, options);
+        this.boxElement.forEach(image => observer.observe(image));
+    }
+
+    handleIntersect(entries, observer) {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting){
+                let randomIndex = Math.floor(Math.random() * 5) + 1;
+                entry.target.setAttribute('src', `./img/${randomIndex}.jpg`.concat());
+            }
+
+        });
+    }
+}
