c3227b29ec49ade1b61973ddd39f2debf70c7572
dahye-program <dahye0729@sch.ac.kr>
2021-10-10 17:44:39 +0900

    [UPDATE] 적용해보쟈

--- client/client.js
+++ client/client.js
@@ -6,33 +6,14 @@ let result;
 
 class Main {
     #dom
-    #table
-    #tableData
-    #childData
 
     boxElement = [];
 
     constructor(dom) {
         this.#dom = dom;
-        this.#table = dom.querySelector(".table");
         this.#insertListener();
         this.#selectListener();
-        this.#prepareTableRowDom();
-
-        this.boxElement = document.querySelectorAll('.table-row');
-    }
-
-    #prepareTableDataDom() {
-        const td_img = document.querySelector('.template-data');
-        const template_ = document.importNode(td_img.content, true);
-        this.#childData = template_.querySelector('.img-data');
-    }
-
-    #prepareTableRowDom() { // 5개씩 출력 위함
-        const t_img = document.querySelector('.template-image');
-        const template = document.importNode(t_img.content, true);
-        this.#tableData = template.querySelector('.table-row');
-        this.#table.appendChild(template);
+        this.boxElement = document.querySelectorAll('.img-data');
     }
 
     #insertListener() { // 이미지 DB에 INSERT
@@ -45,6 +26,14 @@ class Main {
         this.#dom.querySelector('.SELECT').addEventListener('click', async () => {
             await this.#selectEvent();
         })
+    }
+
+    async #selectEvent() {
+        const response = await fetch('http://localhost:3000/img');
+        if (response.status === 200) {
+            result = JSON.parse(await response.text());
+            this.createObserver();
+        }
     }
 
     createObserver() {
@@ -61,29 +50,9 @@ class Main {
     handleIntersect(entries, observer) {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
-                let imageData = result.shift();
-                const loopCount = result.length % 5 === 0 ? result.length / 5 : result.length / 5 + 1;
-                for (let i = 0; i <= Math.floor(loopCount); i++) {
-                    for (let j = 0; j < 5; j++) {
-                        this.#prepareTableDataDom();
-                        const imageDom = this.#childData.querySelector('.Image');
-                        entry.target.setAttribute('src', 'data:image/png;base64, '.concat(imageData.img));
-                        //this.insertImage(imageDom, imageData.img);
-                        this.#tableData.appendChild(this.#childData);
-                        imageData = result.shift();
-                    }
-                    this.#prepareTableRowDom();
-                }
+                entry.target.setAttribute('src', 'data:image/png;base64, '.concat(result.img));
             }
         });
-    }
-
-    async #selectEvent() {
-        const response = await fetch('http://localhost:3000/img');
-        if (response.status === 200) {
-            result = JSON.parse(await response.text());
-            this.createObserver();
-        }
     }
 }
 
